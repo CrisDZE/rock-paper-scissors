df50e05087eb3a799183f9c8b08f748051906b3a
CrisDZE <150843837+CrisDZE@users.noreply.github.com>
2023-11-24 11:07:56 +0100

    UI actualize

--- javascript.js
+++ javascript.js
@@ -1,51 +1,79 @@
-for (let i = 0; i < 5; i++){
-    const playerSelectionOne = prompt("Write your choice!")
-    const playerSelection = playerSelectionOne.toLowerCase()
-    const computerSelection = getComputerChoice();
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const playerDisplay = document.querySelector('#playerResult');
+const computerDisplay = document.querySelector('#computerResult');
+const winnerAnnouncement = document.querySelector('#winnerAnnouncement');
+const reset = document.querySelector('#resetGame')
 
-    function getComputerChoice() {
-        const randomNumber = Math.floor(Math.random() * 3);
+let computerCount = 0;
+let playerCount = 0;
 
-        switch (randomNumber) {
-            case 0:
-                return 'rock';
-            case 1:
-                return 'paper';
-            case 2:
-                return 'scissors';
-        }
+rock.addEventListener('click', () => playRound('rock'));
+paper.addEventListener('click', () => playRound('paper'));
+scissors.addEventListener('click', () => playRound('scissors'));
+reset.addEventListener('click', () => resetGame());
+
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+        
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+};
+
+function playRound(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const winner = determineWinner(playerChoice, computerChoice);
+
+    if (winner === 'player') {
+        playerCount++;
+    } else if (winner === 'computer') {
+        computerCount++;
     }
 
-    function playRound(playerSelection, computerSelection){
-        if (playerSelection === "rock"){
-            if (computerSelection === 'rock'){ 
-                return 'tie!'
-            } else if (computerSelection === 'paper'){
-                return 'Computer wins!'
-            } else if (computerSelection === 'scissors'){
-                return 'Player wins!'
-            }
-        }
-        else if (playerSelection === "paper"){
-            if (computerSelection === 'rock'){ 
-                return 'Player wins!'
-            } else if (computerSelection === 'paper'){
-                return 'tie!'
-            } else if (computerSelection === 'scissors'){
-                return 'Computer wins!'
-            }
-        }
-        else if (playerSelection === "scissors"){
-            if (computerSelection === 'rock'){ 
-                return 'Computer wins!'
-            } else if (computerSelection === 'paper'){
-                return 'Player wins!'
-            } else if (computerSelection === 'scissors'){
-                return 'tie!'
-            }
-        }
-    }
+    updateDisplay();
 
-    alert(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
+    checkWinner();
+}
+
+function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return 'tie';
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        return 'player';
+    } else {
+        return 'computer';
+    }
+};
+
+function updateDisplay() {
+    playerDisplay.textContent = String(playerCount);
+    computerDisplay.textContent = String(computerCount);
+}
+
+function checkWinner() {
+    if (playerCount === 5 && computerCount < 5) {
+        winnerAnnouncement.textContent = '¡El jugador ha ganado!';
+    } else if (computerCount === 5 && playerCount < 5) {
+        winnerAnnouncement.textContent = '¡La computadora ha ganado!';
+    }
+}
+
+function resetGame() {
+    playerCount = 0;
+    computerCount = 0;
+    playerDisplay.textContent =  '0';
+    computerDisplay.textContent = '0';
+    winnerAnnouncement.textContent = ''
 }
